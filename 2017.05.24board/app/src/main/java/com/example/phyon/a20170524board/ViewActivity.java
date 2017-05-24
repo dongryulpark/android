24cd054d254ed62ed62eb469fcd9d23524959c05
dongryul <phyong5226@gmail.com>
2017-05-24 21:56:57 +0900

    add activity

---
+++ 2017.05.24board/app/src/main/java/com/example/phyon/a20170524board/ViewActivity.java
@@ -0,0 +1,18 @@
+package com.example.phyon.a20170524board;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by phyon on 2017-05-24.
+ */
+
+public class ViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boardview);
+
+    }
+}
